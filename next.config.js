f5ea2405774b27baabd8f822af55a83869188c29
poonambudhwar83-design <poonambudhwar83@gmail.com>
2026-09-03 08:19:10 +0530

    Include Chromium binaries in Vercel functions

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  serverExternalPackages: ['@sparticuz/chromium','puppeteer-core']
+  serverExternalPackages: ['@sparticuz/chromium','puppeteer-core'],
+  outputFileTracingIncludes: {
+    '/api/track': ['./node_modules/@sparticuz/chromium/bin/**/*'],
+    '/api/browser-track': ['./node_modules/@sparticuz/chromium/bin/**/*']
+  }
 };
