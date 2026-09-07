f83d3f3ca85adcafe53ad8c92e2f23e41c3d3511
poonambudhwar83-design <poonambudhwar83@gmail.com>
2026-09-07 13:02:11 +0530

    Trace OCR runtime dependency and keep Oman direct extraction stable

--- next.config.js
+++ next.config.js
@@ -1,27 +1,31 @@
 module.exports = {
-  serverExternalPackages: ['@sparticuz/chromium','puppeteer-core','tesseract.js','tesseract.js-core','bmp-js'],
+  serverExternalPackages: ['@sparticuz/chromium','puppeteer-core','tesseract.js','tesseract.js-core','wasm-feature-detect','bmp-js'],
   outputFileTracingIncludes: {
     '/api/track': [
       './node_modules/@sparticuz/chromium/bin/**/*',
       './node_modules/tesseract.js/**/*',
       './node_modules/tesseract.js-core/**/*',
+      './node_modules/wasm-feature-detect/**/*',
       './node_modules/bmp-js/**/*'
     ],
     '/api/browser-track': [
       './node_modules/@sparticuz/chromium/bin/**/*',
       './node_modules/tesseract.js/**/*',
       './node_modules/tesseract.js-core/**/*',
+      './node_modules/wasm-feature-detect/**/*',
       './node_modules/bmp-js/**/*'
     ],
     '/api/cron/refresh': [
       './node_modules/@sparticuz/chromium/bin/**/*',
       './node_modules/tesseract.js/**/*',
       './node_modules/tesseract.js-core/**/*',
+      './node_modules/wasm-feature-detect/**/*',
       './node_modules/bmp-js/**/*'
     ],
     '/api/saudia-screenshot': [
       './node_modules/tesseract.js/**/*',
       './node_modules/tesseract.js-core/**/*',
+      './node_modules/wasm-feature-detect/**/*',
       './node_modules/bmp-js/**/*'
     ]
   }
