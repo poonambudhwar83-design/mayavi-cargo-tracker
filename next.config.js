5c925ac3c811d6804a4c4c1e1d7aaa0e82834286
poonambudhwar83-design <poonambudhwar83@gmail.com>
2026-09-07 07:06:01 +0530

    Trace Saudia screenshot OCR worker files

--- next.config.js
+++ next.config.js
@@ -1,20 +1,28 @@
 module.exports = {
-  serverExternalPackages: ['@sparticuz/chromium','puppeteer-core','tesseract.js','tesseract.js-core'],
+  serverExternalPackages: ['@sparticuz/chromium','puppeteer-core','tesseract.js','tesseract.js-core','bmp-js'],
   outputFileTracingIncludes: {
     '/api/track': [
       './node_modules/@sparticuz/chromium/bin/**/*',
       './node_modules/tesseract.js/**/*',
-      './node_modules/tesseract.js-core/**/*'
+      './node_modules/tesseract.js-core/**/*',
+      './node_modules/bmp-js/**/*'
     ],
     '/api/browser-track': [
       './node_modules/@sparticuz/chromium/bin/**/*',
       './node_modules/tesseract.js/**/*',
-      './node_modules/tesseract.js-core/**/*'
+      './node_modules/tesseract.js-core/**/*',
+      './node_modules/bmp-js/**/*'
     ],
     '/api/cron/refresh': [
       './node_modules/@sparticuz/chromium/bin/**/*',
       './node_modules/tesseract.js/**/*',
-      './node_modules/tesseract.js-core/**/*'
+      './node_modules/tesseract.js-core/**/*',
+      './node_modules/bmp-js/**/*'
+    ],
+    '/api/saudia-screenshot': [
+      './node_modules/tesseract.js/**/*',
+      './node_modules/tesseract.js-core/**/*',
+      './node_modules/bmp-js/**/*'
     ]
   }
 };
