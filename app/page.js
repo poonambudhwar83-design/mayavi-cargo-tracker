732fce049b758ac06a32e87f027db5dcace50aac
poonambudhwar83-design <poonambudhwar83@gmail.com>
2026-09-12 08:59:22 +0530

    Show total weight in active and cleared masters

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import UnifiedDashboard from './UnifiedDashboard.js';
 import ActiveMasterFilters from './ActiveMasterFilters.js';
+import MasterTotals from './MasterTotals.js';
 
 export default function Page(){
-  return <><UnifiedDashboard/><ActiveMasterFilters/></>;
+  return <><UnifiedDashboard/><ActiveMasterFilters/><MasterTotals/></>;
 }
