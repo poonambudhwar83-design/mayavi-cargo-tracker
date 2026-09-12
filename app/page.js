708a96a2672bc04cff5108f851e4b7738ebdec63
poonambudhwar83-design <poonambudhwar83@gmail.com>
2026-09-12 09:00:54 +0530

    Mount customs cleared filters and total weight

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 import UnifiedDashboard from './UnifiedDashboard.js';
 import ActiveMasterFilters from './ActiveMasterFilters.js';
 import MasterTotals from './MasterTotals.js';
+import CustomsClearedEnhancements from './CustomsClearedEnhancements.js';
 
 export default function Page(){
-  return <><UnifiedDashboard/><ActiveMasterFilters/><MasterTotals/></>;
+  return <><UnifiedDashboard/><ActiveMasterFilters/><MasterTotals/><CustomsClearedEnhancements/></>;
 }
