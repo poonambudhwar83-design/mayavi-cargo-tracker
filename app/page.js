037e0ac1e86ee39895c94d67ac45519122d93436
poonambudhwar83-design <poonambudhwar83@gmail.com>
2026-09-13 23:04:55 +0530

    Run automatic IndiGo sync on dashboard

--- app/page.js
+++ app/page.js
@@ -2,7 +2,8 @@ import UnifiedDashboard from './UnifiedDashboard.js';
 import ActiveMasterFilters from './ActiveMasterFilters.js';
 import MasterTotals from './MasterTotals.js';
 import CustomsClearedEnhancements from './CustomsClearedEnhancements.js';
+import IndigoAutoSync from './IndigoAutoSync.js';
 
 export default function Page(){
-  return <><UnifiedDashboard/><ActiveMasterFilters/><MasterTotals/><CustomsClearedEnhancements/></>;
+  return <><IndigoAutoSync/><UnifiedDashboard/><ActiveMasterFilters/><MasterTotals/><CustomsClearedEnhancements/></>;
 }
