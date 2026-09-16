5f35d3fc7410891ddd889fa0ec44112a49f8f64b
poonambudhwar83-design <poonambudhwar83@gmail.com>
2026-09-16 18:23:48 +0530

    Show MAWB entry date after serial number in all dashboard tabs

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ import ActiveMasterFilters from './ActiveMasterFilters.js';
 import MasterTotals from './MasterTotals.js';
 import CustomsClearedEnhancements from './CustomsClearedEnhancements.js';
 import IndigoAutoSync from './IndigoAutoSync.js';
+import EntryDateColumn from './EntryDateColumn.js';
 
 export default function Page(){
-  return <><IndigoAutoSync/><UnifiedDashboard/><ActiveMasterFilters/><MasterTotals/><CustomsClearedEnhancements/></>;
+  return <><IndigoAutoSync/><UnifiedDashboard/><EntryDateColumn/><ActiveMasterFilters/><MasterTotals/><CustomsClearedEnhancements/></>;
 }
