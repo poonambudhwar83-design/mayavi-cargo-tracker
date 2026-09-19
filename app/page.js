d6a41c598e3e7b129844fbec613aa1b2d316e499
poonambudhwar83-design <poonambudhwar83@gmail.com>
2026-09-19 08:56:35 +0530

    Remove active masters entry date range filter

--- app/page.js
+++ app/page.js
@@ -3,8 +3,7 @@ import ActiveMasterFilters from './ActiveMasterFilters.js';
 import MasterTotals from './MasterTotals.js';
 import CustomsClearedEnhancements from './CustomsClearedEnhancements.js';
 import IndigoAutoSync from './IndigoAutoSync.js';
-import EntryDateColumn from './EntryDateColumn.js';
 
 export default function Page(){
-  return <><IndigoAutoSync/><UnifiedDashboard/><EntryDateColumn/><ActiveMasterFilters/><MasterTotals/><CustomsClearedEnhancements/></>;
+  return <><IndigoAutoSync/><UnifiedDashboard/><ActiveMasterFilters/><MasterTotals/><CustomsClearedEnhancements/></>;
 }
