05102a5b34a5fdf8014b2cd69f97291895000d2e
poonambudhwar83-design <poonambudhwar83@gmail.com>
2026-09-21 08:23:10 +0530

    Add robots metadata for Google indexing

---
+++ app/robots.js
@@ -0,0 +1,7 @@
+export default function robots(){
+  return {
+    rules:[{userAgent:'*',allow:'/'}],
+    sitemap:'https://mayavicargo.com/sitemap.xml',
+    host:'https://mayavicargo.com'
+  };
+}
