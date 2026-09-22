5b7731f9e0d418c5cd8c2400ef5b2f2e75b32390
poonambudhwar83-design <poonambudhwar83@gmail.com>
2026-09-22 06:54:37 +0530

    Open IGCSE scanner on branch preview

--- app/page.js
+++ app/page.js
@@ -1,9 +1,2 @@
-import UnifiedDashboard from './UnifiedDashboard.js';
-import ActiveMasterFilters from './ActiveMasterFilters.js';
-import MasterTotals from './MasterTotals.js';
-import CustomsClearedEnhancements from './CustomsClearedEnhancements.js';
-import IndigoAutoSync from './IndigoAutoSync.js';
-
-export default function Page(){
-  return <><IndigoAutoSync/><UnifiedDashboard/><ActiveMasterFilters/><MasterTotals/><CustomsClearedEnhancements/></>;
-}
+import IGCSEProgressScanner from './IGCSEProgressScanner.js';
+export default function Page(){return <IGCSEProgressScanner/>}
